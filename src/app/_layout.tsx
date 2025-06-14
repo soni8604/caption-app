9d8cc901ec0746af2f37cfe541f1fc77abb73239
santoshSoni86 <santosh.soni@legalpay.in>
2025-06-14 12:39:53 +0530

    layout page added

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,53 @@
 import "@/global.css";
-import { Stack } from "expo-router";
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  useFonts,
+} from "@expo-google-fonts/poppins";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Slot, SplashScreen } from "expo-router";
+import { useEffect } from "react";
+import { useColorScheme } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-export default function RootLayout() {
-  return <Stack />;
-}
+SplashScreen.preventAutoHideAsync();
+
+const InitialLayout = () => {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Slot />;
+};
+
+const RootLayout = () => {
+  const colorScheme = useColorScheme();
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <InitialLayout />
+      </ThemeProvider>
+    </GestureHandlerRootView>
+  );
+};
+
+export default RootLayout;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 bg-dark justify-center items-center">
+      <Text className="text-white">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
